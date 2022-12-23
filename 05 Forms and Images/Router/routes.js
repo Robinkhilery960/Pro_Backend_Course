d006d0d5f524209d3af8d6c25d45e7ff89a9bdc9
Robinkhilery960 <robinkhilery960@gmail.com>
2022-12-23 19:39:42 +0530

    single file upload using cloudinary

--- 05 Forms and Images/Router/routes.js
+++ 05 Forms and Images/Router/routes.js
@@ -2,22 +2,35 @@ import express from "express"
 import { getForm } from "../controllers/getForm.js"
 import { Home } from "../controllers/home.js"
 import { postForm } from "../controllers/postForm.js"
-
+import { v2 as cloudinary } from 'cloudinary'
 const router=express.Router()
 
 router.get("/",Home)
+
 router.get("/getform",getForm)
+
 router.get("/postform",postForm)
+
+
 router.get("/myget",(req,res)=>{
+   
     console.log("request object",req)
     console.log(req.body)
     console.log(req.query)
     res.send(req.body)
 })
+
+
 router.post("/mypost", (req,res)=>{
-    console.log(req.files)
+    cloudinary.uploader.upload( 
+        req.files.samplefile.tempFilePath,
+        {folder:"users"}
+    ).then((result)=>console.log(result))
+    .catch(err=>console.log(err));
+    console.log(req.files.samplefile)
     console.log(req.body)
     res.send(req.body)
 })
+
 
 export default router
--- 05 Forms and Images/index.js
+++ 05 Forms and Images/index.js
@@ -1,8 +1,21 @@
- import express from "express"  
+import * as dotenv from 'dotenv'
+import express from "express"  
  import router from './Router/routes.js'
  import fileUpload from "express-fileupload"
-
+ import { v2 as cloudinary } from 'cloudinary'
+dotenv.config()
 const app=express()     
+
+
+cloudinary.config({ 
+    cloud_name:process.env.CLOUDINARY_CLOUD_NAME ,
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+    secure: true
+  }); 
+
+
+
 app.use(fileUpload({
     useTempFiles : true,
     tempFileDir : '/tmp/' 
@@ -10,10 +23,14 @@ app.use(fileUpload({
 app.use(express.json())
 app.use(express.urlencoded({extended:false})) 
 app.use(router)
-app.set("view engine","ejs")
- 
-app.listen(4000,()=>{
-    console.log("server is listening at port 4000")
+app.set("view engine","ejs") 
+
+
+
+
+
+app.listen(process.env.PORT,()=>{
+    console.log(`server is listening at ${process.env.PORT}`) 
 })
 
 export default app
