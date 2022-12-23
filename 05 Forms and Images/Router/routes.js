b8ed1b564683cdfa0f0038ae7a14d5de81f27c47
Robinkhilery960 <robinkhilery960@gmail.com>
2022-12-23 21:49:07 +0530

    cloudinary multiple file upload functionality added

--- 05 Forms and Images/Router/routes.js
+++ 05 Forms and Images/Router/routes.js
@@ -1,36 +1,42 @@
-import express from "express"
-import { getForm } from "../controllers/getForm.js"
-import { Home } from "../controllers/home.js"
-import { postForm } from "../controllers/postForm.js"
-import { v2 as cloudinary } from 'cloudinary'
-const router=express.Router()
+import express from "express";
+import { getForm } from "../controllers/getForm.js";
+import { Home } from "../controllers/home.js";
+import { postForm } from "../controllers/postForm.js";
+import { v2 as cloudinary } from "cloudinary";
+const router = express.Router();
 
-router.get("/",Home)
+router.get("/", Home);
 
-router.get("/getform",getForm)
+router.get("/getform", getForm);
 
-router.get("/postform",postForm)
+router.get("/postform", postForm);
 
+router.get("/myget", (req, res) => {
+  console.log("request object", req);
+  console.log(req.body);
+  console.log(req.query);
+  res.send(req.body);
+});
 
-router.get("/myget",(req,res)=>{
-   
-    console.log("request object",req)
-    console.log(req.body)
-    console.log(req.query)
-    res.send(req.body)
-})
+router.post("/mypost", async (req, res) => {
+    let finalResult=[]
+  for (let i = 0; i < req.files.samplefile.length; i++) {
+    try { 
+        const result = await cloudinary.uploader.upload(
+        req.files.samplefile[i].tempFilePath,
+        { folder: "users" }
+      );
+       
+     finalResult.push(result)
+    } catch (error) { 
+      console.log(error); 
+    }
+  }
+  res.status(200).json({
+    success: true,
+    message: "File uploaded successfully", 
+    finalResult
+  });
+});
 
-
-router.post("/mypost", (req,res)=>{
-    cloudinary.uploader.upload( 
-        req.files.samplefile.tempFilePath,
-        {folder:"users"}
-    ).then((result)=>console.log(result))
-    .catch(err=>console.log(err));
-    console.log(req.files.samplefile)
-    console.log(req.body)
-    res.send(req.body)
-})
-
-
-export default router
+export default router;
